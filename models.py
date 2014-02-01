8d6966a1b6619bca8baa61ff50a8263685f724a3
Yoav Ram <yoavram@gmail.com>
2014-02-02 00:38:35 +0200

    default recipe name, update date field, drop table on main run

--- models.py
+++ models.py
@@ -11,16 +11,22 @@ class Book(Model):
 
 
 class Recipe(Model):
-    name = CharField()
+    name = CharField(default='')
     image = CharField()
     addition_date = DateField(default=datetime.now)
+    update_date = DateField(default=datetime.now)
     book = ForeignKeyField(Book, null=True, related_name="recipes")
     is_reviewed = BooleanField(default=False)
+    #tags = 
+    #ingridients = 
+
 
     class Meta:
 		database = database # this model uses the in-memory database we just created
 
 
 if __name__ == '__main__':
+	Recipe.drop_table()
 	Recipe.create_table()
+	Book.drop_table()
 	Book.create_table()
