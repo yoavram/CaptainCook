a3a48848902ecebfaa72f0ef0facbe37aede68c5
Yoav Ram <yoavram@gmail.com>
2014-02-02 00:06:33 +0200

    watches folder and creates record for new files

--- import.py
+++ import.py
@@ -1,13 +1,28 @@
 import time
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
-import logging
+from watchdog.events import FileSystemEventHandler
+from common import *
+from models import *
+from os.path import basename
+
+class Importer(FileSystemEventHandler):
+    def on_created(self, event):
+        if event.is_directory:
+            logging.warn("No support for folder importing!")
+            return
+        if event.event_type != 'created':
+            raise Exception("Expected a creation event.")
+        
+        path = basename(event.src_path)
+        try:
+            Recipe.get(Recipe.image==path)
+        except DoesNotExist:
+            recipe = Recipe.create(name='', image=path)
+            logging.info("New recipe in file %s", path)
+
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    event_handler = LoggingEventHandler()
+    event_handler = Importer()
     observer = Observer()
     observer.schedule(event_handler, path='import', recursive=True)
     observer.start()
